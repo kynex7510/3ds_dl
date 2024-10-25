838434fa0e5583bd1c73c22c93282c5fb7652de8
kynex7510 <nikkitten3@gmail.com>
2024-10-25 16:14:19 +0200

    Update error

--- Source/Error.c
+++ Source/Error.c
@@ -1,50 +1,53 @@
-#include <3ds/synchronization.h>
-
-#include "dlfcn.h"
 #include "Error.h"
 
-// Globals
+static __thread size_t g_LastError = (size_t)Err_OK;
 
-static const char *ERRORS[MAX_ERRORS] = {
-    "invalid parameter",
-    "could not read input file",
-    "no memory",
-    "hit the handle limit",
-    "hit the reference limit",
-    "not found",
-    "invalid object",
-    "the object is not 32-bit",
-    "the object is not position indipendent",
-    "unknown architecture",
-    "could not map object",
-    "path too long",
-    "relocation failed",
-    "too many dependencies",
-    "could not load dependency",
-    "could not unload object",
-};
-
-static __thread size_t g_LastError = MAX_ERRORS;
-
-// Error
-
-size_t ctrdl_getLastError() {
-  size_t error = g_LastError;
-  ctrdl_resetLastError();
-  return error;
+CTRDLError ctrdl_getLastError(void) {
+	const CTRDLError error = (CTRDLError)g_LastError;
+	ctrdl_resetLastError();
+	return error;
 }
 
-void ctrdl_setLastError(const size_t error) { g_LastError = error; }
+void ctrdl_setLastError(CTRDLError error) { g_LastError = (size_t)error; }
+void ctrdl_resetLastError(void) { ctrdl_setLastError(Err_OK); }
 
-void ctrdl_resetLastError() { ctrdl_setLastError(MAX_ERRORS); }
+const char* ctrdl_getErrorAsString(CTRDLError error) {
+	switch (error) {
+		case Err_InvalidParam:
+			return "invalid parameter";
+		case Err_ReadFailed:
+			return "could not read input file";
+		case Err_NoMemory:
+			return "no memory";
+		case Err_HandleLimit:
+			return "hit the handle limit";
+		case Err_RefLimit:
+			return "hit the reference limit";
+		case Err_NotFound:
+			return "not found";
+		case Err_InvalidObject:
+			return "invalid object";
+		case Err_InvalidBit:
+			return "the object is not 32-bit";
+		case Err_NotPIE:
+			return "the object is not position indipendent";
+		case Err_InvalidArch:
+			return "invalid architecture";
+		case Err_MapError:
+			return "could not map object";
+		case Err_LargePath:
+			return "too large path";
+		case Err_RelocFailed:
+			return "relocation failed";
+		case Err_DepsLimit:
+			return "too many dependencies";
+		case Err_DepFailed:
+			return "could not load dependency";
+		case Err_FreeFailed:
+			return "could not unload object";
+	};
 
-const char *ctrdl_getErrorAsString(const size_t index) {
-  if (index < MAX_ERRORS)
-    return ERRORS[index];
-
-  return NULL;
+	return NULL;
 }
-
-// API
 
 const char *dlerror() { return ctrdl_getErrorAsString(ctrdl_getLastError()); }
--- Source/Error.h
+++ Source/Error.h
@@ -1,29 +1,31 @@
-#ifndef _3DS_DL_ERROR_H
-#define _3DS_DL_ERROR_H
+#ifndef _CTRDL_ERROR_H
+#define _CTRDL_ERROR_H
 
-#include <stdint.h>
+#include <dlfcn.h>
 
-#define ERR_INVALID_PARAM 0
-#define ERR_READ_FAILED 1
-#define ERR_NO_MEMORY 2
-#define ERR_HANDLE_LIMIT 3
-#define ERR_REF_LIMIT 4
-#define ERR_NOT_FOUND 5
-#define ERR_INVALID_OBJECT 6
-#define ERR_INVALID_BIT 7
-#define ERR_NOT_PIE 8
-#define ERR_INVALID_ARCH 9
-#define ERR_MAP_ERROR 10
-#define ERR_BIG_PATH 11
-#define ERR_RELOC_FAIL 12
-#define ERR_DEPS_LIMIT 13
-#define ERR_DEP_FAIL 14
-#define ERR_FREE_FAILED 15
-#define MAX_ERRORS 16
+typedef enum {
+    Err_OK,
+    Err_InvalidParam,
+    Err_ReadFailed,
+    Err_NoMemory,
+    Err_HandleLimit,
+    Err_RefLimit,
+    Err_NotFound,
+    Err_InvalidObject,
+    Err_InvalidBit,
+    Err_NotPIE,
+    Err_InvalidArch,
+    Err_MapError,
+    Err_LargePath,
+    Err_RelocFailed,
+    Err_DepsLimit,
+    Err_DepFailed,
+    Err_FreeFailed,
+} CTRDLError;
 
-size_t ctrdl_getLastError();
-void ctrdl_setLastError(const size_t error);
-void ctrdl_resetLastError();
-const char *ctrdl_getErrorAsString(const size_t index);
+CTRDLError ctrdl_getLastError(void);
+void ctrdl_setLastError(CTRDLError error);
+void ctrdl_resetLastError(void);
+const char* ctrdl_getErrorAsString(CTRDLError error);
 
-#endif /* _3DS_DL_ERROR_H */
+#endif /* _CTRDL_ERROR_H */
